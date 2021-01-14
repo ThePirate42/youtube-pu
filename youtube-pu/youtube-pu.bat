f9405508c84dee098ad2cc1c21e07df3ca111454
ThePirate42 <thepirate.42@outlook.com>
2021-01-14 16:47:12 +0100

    Extended EnableDelayedExpansion to all the script.

--- youtube-pu/youtube-pu.bat
+++ youtube-pu/youtube-pu.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 :: Youtube-pu
 :: By ThePirate42, 2020
 :: Source: https://github.com/ThePirate42/youtube-pu
@@ -9,44 +9,42 @@ set _version=git
 
 echo Loading...
 
-if NOT exist %~dp0youtube-pu.conf echo youtube-pu.conf not found! & goto :end
-setlocal EnableDelayedExpansion
+if NOT exist %~dp0youtube-pu.conf echo youtube-pu.conf not found^^! & goto :end
 for /F "tokens=*" %%g in (%~dp0youtube-pu.conf) do (%%g)
-endlocal & set "_buildpath=%_buildpath%" & set "_default_youtube-dl_path=%_default_youtube-dl_path%" & set "_enable_cmdow=%_enable_cmdow%" & set "_update_pyinstaller=%_update_pyinstaller%" & set "_pull_patches=%_pull_patches%" & set "_python_version=%_python_version%" & set "_check_youtube-pu_version=%_check_youtube-pu_version%"
 
 :: Dependency check
 
 for /F "tokens=*" %%g in ('where jq 2^> nul') do (set _jqpath=%%g)
-if ""=="%_jqpath%" echo jq not found! & goto :end
+if ""=="%_jqpath%" echo jq not found^^! & goto :end
 for /F "tokens=*" %%g in ('where curl 2^> nul') do (set _curlpath=%%g)
-if ""=="%_curlpath%" echo curl not found! & goto :end
+if ""=="%_curlpath%" echo curl not found^^! & goto :end
 for /F "tokens=*" %%g in ('where git 2^> nul') do (set _gitpath=%%g)
-if ""=="%_gitpath%" echo git not found! & goto :end
+if ""=="%_gitpath%" echo git not found^^! & goto :end
 for /F "tokens=*" %%g in ('where py 2^> nul') do (set _pypath=%%g)
-if ""=="%_pypath%" echo py not found! & goto :end
-setlocal EnableDelayedExpansion
+if ""=="%_pypath%" echo py not found^^! & goto :end
 if NOT ""=="%_python_version%" (
 set "_pyver= -%_python_version%"
 for /F "tokens=*" %%g in ('py!_pyver! -c "print(""test"")" 2^> nul') do (set _python_test=%%g)
 if NOT "test"=="!_python_test!" echo Python v%_python_version% not found^^! & goto :end
 )
-endlocal & set "_pyver=%_pyver%"
 for /F "tokens=*" %%g in ('py%_pyver% -c "import os, sys; print(os.path.dirname(sys.executable))"') do (set _pyinstallerpath=%%g\Scripts\pyinstaller.exe)
-if NOT "true"=="%_update_pyinstaller%" (if NOT exist "%_pyinstallerpath%" echo pyinstaller not found! & goto :end )
+if NOT "true"=="%_update_pyinstaller%" (if NOT exist "%_pyinstallerpath%" echo pyinstaller not found^^! & goto :end )
 for /F "tokens=*" %%g in ('where cmdow 2^> nul') do (set _cmdowpath=%%g)
-if "true"=="%_enable_cmdow%" (if ""=="%_cmdowpath%" echo cmdow not found! & goto :end )
-if exist "%_buildpath%" echo The build folder already exists! & goto :end
-
-<nul set /p ="A[2K"
+if "true"=="%_enable_cmdow%" (if ""=="%_cmdowpath%" echo cmdow not found^^! & goto :end )
+if exist "%_buildpath%" echo The build folder already exists^^! & goto :end
 
 :: "cmdow @ /MAX" maximizes the window, you can disabe it from the configuration
 
 if "true"=="%_enable_cmdow%" cmdow @ /MAX
 
+:: Removes "Loading..." text
+
+<nul set /p ="A[2K"
+
 :: Check for youtube-pu updates
 
 for /F "tokens=*" %%g in ('curl -sS -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/thepirate42/youtube-pu/releases/latest ^| jq -r .tag_name') do (set _currentscriptversion=%%g)
-if "true"=="%_check_youtube-pu_version%" (if NOT %_version%==git (if NOT %_version%==%_currentscriptversion% echo A new youtube-pu version [%_currentscriptversion%] is available!))
+if "true"=="%_check_youtube-pu_version%" (if NOT %_version%==git (if NOT %_version%==%_currentscriptversion% echo A new youtube-pu version [%_currentscriptversion%] is available^^!))
 
 :: I know, the following code contains too much goto, sorry!
 
@@ -64,7 +62,7 @@ if %ERRORLEVEL% EQU 2 goto :end
 goto :build
 :update
 set _installmode=1
-echo A new version is available! (%_localversion% --^> %_currentversion%)
+echo A new version is available^^! (%_localversion% --^> %_currentversion%)
 choice /M "Do you want to update"
 if %ERRORLEVEL% EQU 2 goto :end
 goto :build
@@ -76,7 +74,6 @@ git clone -b %_currentversion% https://github.com/ytdl-org/youtube-dl.git "%_bui
 if NOT ""=="%_pull_patches%" (
 echo [7mPatching...[0m
 pushd "%_buildpath%\%_sourcefoldername%"
-setlocal EnableDelayedExpansion
 echo:
 for %%g in ("%_pull_patches:_=" "%") do (
 set _mergemsg=merged
@@ -88,7 +85,6 @@ echo [33mWARNING: %%~g is already merged in the master branch, skipping.[0m
 )
 echo:
 )
-endlocal
 popd
 )
 echo [7mCompiling...[0m
@@ -102,7 +98,7 @@ if NOT "%_currentversion%" == "%_newversion%" goto :problem
 echo [7mFinal operations...[0m
 move /Y "%_buildpath%\dist\youtube-dl.exe" "%_localpath%" > nul
 rd /S /Q "%_buildpath%"
-if %_installmode% == 1 (echo Update completed!) else (echo Installation completed!)
+if %_installmode% == 1 (echo Update completed^^!) else (echo Installation completed^^!)
 goto :end
 :problem
 echo [41;97mSomething has gone wrong :-([0m
@@ -112,7 +108,7 @@ rd /S /Q "%_buildpath%"
 goto :end
 :noupdate
 set _installmode=2
-echo youtube-dl is up-to-date! (%_currentversion%)
+echo youtube-dl is up-to-date^^! (%_currentversion%)
 goto :end
 :end
 pause
